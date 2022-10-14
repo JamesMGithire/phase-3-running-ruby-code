eaa2aedbddb8c849c7535b0845c4b0fd0cc2304d
James Mburu Githire <jamesmburug24@gmail.com>
2022-10-14 19:06:30 +0300

    Completed assignment

--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
 # Print outputs on one line
-print 'Hello world!'
-print 'Hello world!'
-print 'Hello world!'
+puts 'Hello World!'
+print "Pass this test, please."
+# puts 'Hello world!'
+# puts 'Hello world!'
 # puts outputs each item on its own line
 # When using puts on something that isn't already a string,
 # Ruby will coerce the data to a string by calling the ".to_s method".
@@ -10,8 +11,9 @@ puts [1, 2, 3]
 # The p method will output the data in a nicer format by calling the ".inspect"
 p [1, 2, 3]
 # The pp method will "pretty-print" these objects by calling the ".pretty_inspect"
-pp [{ id: 1, hello: 'World' }, { id: 2, hello: 'Ruby' }, { id: 3, hello: 'Moon' }, { id: 4, hello: 'Learner' }]
+# pp [{ id: 1, hello: 'World' }, { id: 2, hello: 'Ruby' }, { id: 3, hello: 'Moon' }, { id: 4, hello: 'Learner' }]
 
 # IRB
 #  The first line is the output of the puts method;
 # the second line is the return value of the puts method.
+# 
